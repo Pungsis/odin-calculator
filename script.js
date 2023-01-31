d1e18181fa2997d94830f0b3a1fc594aea822dc6
Tomas <102042076+Pungsis@users.noreply.github.com>
2023-01-31 10:48:26 -0300

    add more functionalities and handling errors

--- script.js
+++ script.js
@@ -17,9 +17,30 @@ function divide(a, b) {
 
 const operate = (operator, a, b) => operator(a, b);
 
-let first, second, operator;
+let first,
+ second,
+ operator,
+ isPressed = false,
+ resultado,
+ valor;
 
 d.addEventListener("click", (e) => {
+ if (e.target.value === "sign") {
+  // como cambio de - a " "? esto no sirve !!!
+  if (isPressed) {
+   $result2.prepend("-");
+  }
+
+  if (!isPressed) {
+   console.log($result2.textContent);
+   $result2.textContent = $result2.textContent.replace("-", "");
+   $result2.prepend("");
+  }
+  isPressed ? (isPressed = false) : (isPressed = true);
+
+  console.log(isPressed);
+ }
+
  if (e.target.classList.contains("number")) {
   $result2.textContent += e.target.value;
  }
@@ -28,7 +49,11 @@ d.addEventListener("click", (e) => {
   e.target.classList.contains("operator") &&
   $result2.textContent != ""
  ) {
-  first = parseInt($result2.textContent);
+  isPressed
+   ? (first = parseFloat($result2.textContent) * -1)
+   : (first = parseFloat($result2.textContent));
+  console.log(first);
+
   operator = e.target.id;
   $result.textContent = `${$result2.textContent} ${e.target.value}`;
   $result2.textContent = "";
@@ -44,9 +69,30 @@ d.addEventListener("click", (e) => {
  }
 
  if (e.target.classList.contains("igual")) {
-  second = parseInt($result2.textContent);
-  $result.textContent = `${$result.textContent} ${second} =`;
+  if (
+   $result2.textContent.length == 0 &&
+   $result.textContent.length > 0
+  ) {
+   console.log("hola");
+   $result.textContent = "Error: missing argument";
+  }
+  if (
+   $result2.textContent.length > 0 &&
+   $result.textContent.length > 0
+  ) {
+   second = parseFloat($result2.textContent);
+   $result.textContent = `${$result.textContent} ${second} =`;
+   $result2.textContent = "";
+   resultado = operate(window[operator], first, second);
+   $result2.textContent = resultado;
+   first = resultado;
+  }
+ }
+
+ if (e.target.value === "AC") {
   $result2.textContent = "";
-  $result2.textContent = operate(window[operator], first, second);
+  $result.textContent = "";
+  first = undefined;
+  second = undefined;
  }
 });
